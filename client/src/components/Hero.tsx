522804a61172d35aa1a23eead6167b0438adfcce
Maximiliano Jesús Meza <135179123+cocomeza@users.noreply.github.com>
2025-09-27 15:05:14 -0300

    Update Hero.tsx

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap'
 
 export default function Hero() {
   // Imagen original enviada por el usuario (ahora en public)
-  const heroImage = '/fotoHero.jpeg'
+  const heroImage = '/fotoHero.jpg'
   const bg = `url(${heroImage})`
   return (
     <div className="hero" style={{ backgroundImage: bg, backgroundSize: 'cover', backgroundPosition: 'center' }}>
